dde0efbce59105514e39ee0a61a2423871d88102
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-02 20:35:37 +0100

    Added value user input to unit converter

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,22 +1,26 @@
-let number = 20
+let number = document.getElementById("number")
 let value = document.getElementById("value")
-value.textContent = number
 
 let conversion1 = document.getElementById("m-ft")
-let sum1 = number * 3.28084
-let sum2 = number / 3.28084
-conversion1.textContent =  number + " meters = " + sum1.toFixed(3) + " feet | "
-                            + number + " feet = " + sum2.toFixed(3) + " meters"
-
-
 let conversion2 = document.getElementById("l-gal")
-let sum3 = number * 0.264172
-let sum4 = number / 0.264172
-conversion2.textContent =  number + " liters = " + sum3.toFixed(3) + " gallons | "
-                            + number + " gallons = " + sum4.toFixed(3) + " liters"
-
 let conversion3 = document.getElementById("kg-lbs")
-let sum5 = number * 2.2046
-let sum6 = number / 2.2046
-conversion3.textContent =  number + " kilos = " + sum5.toFixed(3) + " pounds | "
-                            + number + " pounds = " + sum6.toFixed(3) + " kilos"
+
+function submit() {
+    console.log(number.value)
+    value.textContent = "Input number: " + number.value
+
+    let sum1 = number.value * 3.28084
+    let sum2 = number.value / 3.28084
+    conversion1.textContent =  number.value + " meters = " + sum1.toFixed(3) + " feet | "
+                                + number.value + " feet = " + sum2.toFixed(3) + " meters"
+    
+    let sum3 = number.value * 0.264172
+    let sum4 = number.value / 0.264172
+    conversion2.textContent =  number.value + " liters = " + sum3.toFixed(3) + " gallons | "
+                                + number.value + " gallons = " + sum4.toFixed(3) + " liters"
+           
+    let sum5 = number.value * 2.2046
+    let sum6 = number.value / 2.2046
+    conversion3.textContent =  number.value + " kilos = " + sum5.toFixed(3) + " pounds | "
+                                + number.value + " pounds = " + sum6.toFixed(3) + " kilos"
+}
