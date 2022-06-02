a29d7ccb72f27b2a67ee9980fed5c27a8de9b8a6
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-02 16:14:44 +0100

    Completed passenger counter application

---
+++ passenger-counter/index.js
@@ -0,0 +1,19 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    console.log("Increment button clicked")
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    console.log("Save button clicked")
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = 0
+}
+
+console.log("Let's count people on the subway!")
