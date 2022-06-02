39f27a2283a39a8a6caea07c13e809a2f14196ed
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-02 21:02:30 +0100

    Added basic styling to input form

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -7,20 +7,22 @@ let conversion3 = document.getElementById("kg-lbs")
 
 function submit() {
     console.log(number.value)
-    value.textContent = "Input number: " + number.value
+    if(!isNaN(number.value)){ // doesn't work for empty string
+        value.textContent = number.value
 
-    let sum1 = number.value * 3.28084
-    let sum2 = number.value / 3.28084
-    conversion1.textContent =  number.value + " meters = " + sum1.toFixed(3) + " feet | "
-                                + number.value + " feet = " + sum2.toFixed(3) + " meters"
+        let sum1 = number.value * 3.28084
+        let sum2 = number.value / 3.28084
+        conversion1.textContent =  number.value + " meters = " + sum1.toFixed(3) + " feet | "
+                                    + number.value + " feet = " + sum2.toFixed(3) + " meters"
     
-    let sum3 = number.value * 0.264172
-    let sum4 = number.value / 0.264172
-    conversion2.textContent =  number.value + " liters = " + sum3.toFixed(3) + " gallons | "
-                                + number.value + " gallons = " + sum4.toFixed(3) + " liters"
+        let sum3 = number.value * 0.264172
+        let sum4 = number.value / 0.264172
+        conversion2.textContent =  number.value + " liters = " + sum3.toFixed(3) + " gallons | "
+                                    + number.value + " gallons = " + sum4.toFixed(3) + " liters"
            
-    let sum5 = number.value * 2.2046
-    let sum6 = number.value / 2.2046
-    conversion3.textContent =  number.value + " kilos = " + sum5.toFixed(3) + " pounds | "
-                                + number.value + " pounds = " + sum6.toFixed(3) + " kilos"
+        let sum5 = number.value * 2.2046
+        let sum6 = number.value / 2.2046
+        conversion3.textContent =  number.value + " kilos = " + sum5.toFixed(3) + " pounds | "
+                                    + number.value + " pounds = " + sum6.toFixed(3) + " kilos"
+    }
 }
