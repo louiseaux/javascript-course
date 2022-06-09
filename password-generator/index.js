45629baa52346290873ff080fecd1e755cf0d23c
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-09 12:16:36 +0100

    Generate two random passwords when button clicked

--- password-generator/index.js
+++ password-generator/index.js
@@ -3,3 +3,24 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
 "$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let password1El = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+
+function generateRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * characters.length)
+    return randomNumber
+}
+
+function generatePassword() {
+    let password = ""
+    for ( let i = 0; i < 15; i++ ) {
+        password += characters[generateRandomNumber()]
+    }
+    return password
+}
+
+function getPasswords() {
+    password1El.textContent = generatePassword()
+    password2El.textContent = generatePassword()
+}
