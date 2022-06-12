235b01c0b2749c894e0275487d9f3aca1c64a41e
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-12 14:15:01 +0100

    Added ability to increase scores

---
+++ basketball-scoreboard/index.js
@@ -0,0 +1,15 @@
+let homeScore = 0
+let guestScore = 0
+let homeEl = document.getElementById("home-el")
+let guestEl = document.getElementById("guest-el")
+
+function addScore(id) {
+    let button = document.getElementById(id).textContent
+    if (id.includes("home")) {
+        homeScore += parseInt(button)
+        homeEl.textContent = homeScore
+    } else {
+        guestScore += parseInt(button)
+        guestEl.textContent = guestScore
+    }
+}
