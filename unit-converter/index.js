211cddd7169a361eaaa1dbe3b1d50828a9a0486c
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-13 01:28:53 +0100

    Final version

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,21 +1,21 @@
-let num = document.getElementById("num").textContent
+let num = document.getElementById("num")
+num.value = 0
 let convertBtn = document.getElementById("convert-btn")
-
 let conversion1 = document.getElementById("m-ft")
 let conversion2 = document.getElementById("l-gal")
 let conversion3 = document.getElementById("kg-lbs")
 
 convertBtn.addEventListener("click", function() {
-    let sum1 = num * 3.28084
-    let sum2 = num / 3.28084
-    conversion1.innerHTML =  `${num} meters = ${sum1.toFixed(3)} feet | ${num} feet = ${sum2.toFixed(3)} meters`
+    let sum1 = num.value * 3.28084
+    let sum2 = num.value / 3.28084
+    conversion1.innerHTML =  `${num.value} meters = ${sum1.toFixed(3)} feet | ${num.value} feet = ${sum2.toFixed(3)} meters`
     
-    let sum3 = num * 0.264172
-    let sum4 = num / 0.264172
-    conversion2.innerHTML =  `${num} liters = ${sum3.toFixed(3)} gallons | ${num} gallons = ${sum4.toFixed(3)} liters`
+    let sum3 = num.value * 0.264172
+    let sum4 = num.value / 0.264172
+    conversion2.innerHTML =  `${num.value} liters = ${sum3.toFixed(3)} gallons | ${num.value} gallons = ${sum4.toFixed(3)} liters`
           
-    let sum5 = num * 2.2046
-    let sum6 = num / 2.2046
-    conversion3.innerHTML =  `${num} kilos = ${sum5.toFixed(3)} pounds | ${num} pounds = ${sum6.toFixed(3)} kilos`
+    let sum5 = num.value * 2.2046
+    let sum6 = num.value / 2.2046
+    conversion3.innerHTML =  `${num.value} kilos = ${sum5.toFixed(3)} pounds | ${num.value} pounds = ${sum6.toFixed(3)} kilos`
     
 })
