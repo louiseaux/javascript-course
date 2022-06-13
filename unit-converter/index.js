87991fc84f6d2a284834292b7b957bb7837b9d4d
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-13 01:17:51 +0100

    Updated styling

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,28 +1,21 @@
-let number = document.getElementById("number")
-let value = document.getElementById("value")
+let num = document.getElementById("num").textContent
+let convertBtn = document.getElementById("convert-btn")
 
 let conversion1 = document.getElementById("m-ft")
 let conversion2 = document.getElementById("l-gal")
 let conversion3 = document.getElementById("kg-lbs")
 
-function submit() {
-    console.log(number.value)
-    if(!isNaN(number.value)){ // doesn't work for empty string
-        value.textContent = number.value
-
-        let sum1 = number.value * 3.28084
-        let sum2 = number.value / 3.28084
-        conversion1.textContent =  number.value + " meters = " + sum1.toFixed(3) + " feet | "
-                                    + number.value + " feet = " + sum2.toFixed(3) + " meters"
+convertBtn.addEventListener("click", function() {
+    let sum1 = num * 3.28084
+    let sum2 = num / 3.28084
+    conversion1.innerHTML =  `${num} meters = ${sum1.toFixed(3)} feet | ${num} feet = ${sum2.toFixed(3)} meters`
     
-        let sum3 = number.value * 0.264172
-        let sum4 = number.value / 0.264172
-        conversion2.textContent =  number.value + " liters = " + sum3.toFixed(3) + " gallons | "
-                                    + number.value + " gallons = " + sum4.toFixed(3) + " liters"
-           
-        let sum5 = number.value * 2.2046
-        let sum6 = number.value / 2.2046
-        conversion3.textContent =  number.value + " kilos = " + sum5.toFixed(3) + " pounds | "
-                                    + number.value + " pounds = " + sum6.toFixed(3) + " kilos"
-    }
-}
+    let sum3 = num * 0.264172
+    let sum4 = num / 0.264172
+    conversion2.innerHTML =  `${num} liters = ${sum3.toFixed(3)} gallons | ${num} gallons = ${sum4.toFixed(3)} liters`
+          
+    let sum5 = num * 2.2046
+    let sum6 = num / 2.2046
+    conversion3.innerHTML =  `${num} kilos = ${sum5.toFixed(3)} pounds | ${num} pounds = ${sum6.toFixed(3)} kilos`
+    
+})
