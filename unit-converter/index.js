812e66b68ceb1732ba56acfbfeb54f9cf8486904
Ashlyn <ashlyn.barry1@gmail.com>
2025-07-17 17:51:34 +1000

    refactor unit converter

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,21 +1,17 @@
-let num = document.getElementById("num")
-num.value = 0
 let convertBtn = document.getElementById("convert-btn")
-let conversion1 = document.getElementById("m-ft")
-let conversion2 = document.getElementById("l-gal")
-let conversion3 = document.getElementById("kg-lbs")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
 
 convertBtn.addEventListener("click", function() {
-    let sum1 = num.value * 3.28084
-    let sum2 = num.value / 3.28084
-    conversion1.innerHTML =  `${num.value} meters = ${sum1.toFixed(3)} feet | ${num.value} feet = ${sum2.toFixed(3)} meters`
-    
-    let sum3 = num.value * 0.264172
-    let sum4 = num.value / 0.264172
-    conversion2.innerHTML =  `${num.value} liters = ${sum3.toFixed(3)} gallons | ${num.value} gallons = ${sum4.toFixed(3)} liters`
-          
-    let sum5 = num.value * 2.2046
-    let sum6 = num.value / 2.2046
-    conversion3.innerHTML =  `${num.value} kilos = ${sum5.toFixed(3)} pounds | ${num.value} pounds = ${sum6.toFixed(3)} kilos`
-    
+    const baseValue = inputEl.value
+
+    lengthEl.textContent =  `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`
+    volumeEl.innerHTML =  `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`
+    massEl.innerHTML =  `${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilos`
 })
