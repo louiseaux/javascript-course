a6c9627ad508c49abf6f437f2808dec7147907e1
Ashlyn <ashlyn.barry1@gmail.com>
2025-08-22 14:05:35 +1000

    added movie search feature

---
+++ movie-watchlist/index.js
@@ -0,0 +1,88 @@
+let myWatchlist = []
+const apiKey = 'bf165cc7'
+const searchForm = document.getElementById('search-form')
+const mySearch = document.getElementById('my-search')
+const movieContainer = document.getElementById('movies')
+const watchlistContainer = document.getElementById('watchlist')
+const moviesFromLocalStorage = JSON.parse( localStorage.getItem("myWatchlist") )
+
+if (moviesFromLocalStorage) {
+    myWatchlist = moviesFromLocalStorage
+    renderWatchlist(myWatchlist)
+}
+
+document.addEventListener('click', function(e) {
+    if (e.target.dataset.add) {
+        handleAddClick(e.target.dataset.add)
+    }
+})
+
+if (searchForm) {
+    searchForm.addEventListener('submit', function(e) {
+        e.preventDefault()
+        handleSearchClick()
+    })
+}
+
+function handleAddClick(movieId) {
+    if (!myWatchlist.includes(movieId)) {
+        console.log(movieId)
+        myWatchlist.push(movieId)
+        localStorage.setItem("myWatchlist", JSON.stringify(myWatchlist))
+    }
+}
+
+function renderWatchlist(watchlist) {
+    if (watchlistContainer) {
+        watchlistContainer.innerHTML = ""
+        for (let movieId of watchlist) {
+            fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        watchlistContainer.innerHTML += getMovieHtml(data)
+                    })
+        }
+    }
+}
+
+function handleSearchClick() {
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${mySearch.value}&type=movie`)
+        .then(res => res.json())
+        .then(data => {
+            movieContainer.innerHTML = ""
+            for (let movie of data.Search) {
+                fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.Poster !== 'N/A') {
+                            movieContainer.innerHTML += getMovieHtml(data)
+                        }
+                    })
+            }
+        })
+    
+}
+
+function getMovieHtml(movie) {
+    return `
+        <div class="movie">
+            <img src="${movie.Poster}" class="movie-img">
+            <div>
+                <div class="movie-header">
+                    <h3>${movie.Title}</h3>
+                    <h4 class="movie-rating">⭐ ${movie.imdbRating}</h4>
+                </div>
+                <div class="movie-info">
+                    <h4>${movie.Runtime}</h4>
+                    <h4>${movie.Genre}</h4>
+                    <span class="watchlist-btn" id="watchlist-btn"
+                        data-add=${movie.imdbID}>
+                        <i class="fa-solid fa-circle-plus"
+                        data-add=${movie.imdbID}></i>
+                        Watchlist
+                    </span>
+                </div>
+                <p class="movie-plot">${movie.Plot}</p>
+            </div>
+        </div>`
+}
